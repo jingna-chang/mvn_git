e04d4286fca805f014d3ba971325cf287d444b33
cjn <847368808@qq.com>
2021-06-27 23:01:30 +0800

    second

---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,12 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+
+    @Test
+    public void testLogin(){
+        System.out.println("登录成功");
+    }
+}
---
+++ src/test/java/testcases/RegisterTest.java
@@ -0,0 +1,11 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class RegisterTest {
+
+    @Test
+    public void testRegister(){
+        System.out.println("注册成功");
+    }
+}
